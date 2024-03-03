c7f7fa82a1d48c9ee95c56464828b5ba3d36f45f
anastasiiaskliar <158153071+anastasiiaskliar@users.noreply.github.com>
2024-03-03 18:44:59 +0200

    c

--- main1.py
+++ main1.py
@@ -1,8 +1,8 @@
 def vivaMechMat():
-    return "Viva Mech-mat faculty!"
+    return "Viva Mech-mat faculty!!!"
 print(vivaMechMat())
 print ("Hello, world")
 def Hello():
    return("Hello,world!")
 for i in range(10):
-   print(Hello())gi
+   print(Hello())
