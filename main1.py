902fa2f15aeb99d1af4aee8cdfca70c79c09c6e4
anastasiiaskliar <158153071+anastasiiaskliar@users.noreply.github.com>
2024-03-03 18:41:40 +0200

    Printing massage "hello world" 10 times

---
+++ main1.py
@@ -0,0 +1,12 @@
+#def vivaMechMat():
+ #   return "Viva Mech-mat faculty!"
+#print(vivaMechMat())
+#def Hello():
+#    return("Hello,world!")
+#for i in range(10):
+#    print(Hello())
+print ("Hello, world")
+def Hello():
+   return("Hello,world!")
+for i in range(10):
+   print(Hello())gi
